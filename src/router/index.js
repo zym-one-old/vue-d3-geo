76ac2eeb623a8e6003b27e6952afa178c075b90a
yiming-zeng <yiming.zeng97@gmail.com>
2020-03-05 19:52:56 +0800

    tailwind

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+const NProgress = require('nprogress')
+import 'nprogress/nprogress.css'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -13,15 +16,24 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+
+NProgress.configure({ showSpinner: false })
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
